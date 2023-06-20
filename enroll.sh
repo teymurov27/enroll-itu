89df3b8070149bda2072e375ca7283affeaae7b8
Samir Teymurov <samir.teymurov@btsgrp.com>
2023-06-20 12:41:47 +0300

    chore: refactor

--- enroll.sh
+++ enroll.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-url="https://kepler-beta.itu.edu.tr/api/TaslakKontrolAPI/v1/" # use this url while you are testing
+# Define the base URL for the API
+base_url="https://kepler-beta.itu.edu.tr/api"
 
-# url="https://kepler-beta.itu.edu.tr/api/ders-kayit/v21/"     # uncomment this line when you are ready
+# Define the API endpoints
+test_endpoint="/TaslakKontrolAPI/v1/"
+prod_endpoint="/ders-kayit/v21/"
 
-# get this token from course enrollment page as shown in the screenshot and paste it here
-# it should go like this:
-# token="Authorization: Bearer eyJhbGciO..." 
-token="paste your token here" 
+# Define the token
+token="$token"
 
-# write the crn numbers of the lectures you want to take. crn's should be separated by space.
-crn_list=("20034" "20289") 
+# Define the CRN list
+crn_list=("23169" "24546")
 
-# Done! Now you can run and test the script. 
-# It should return a JSON response if you made everything correct.
-
-
+# Function to create a JSON array from the CRN list
 json_array() {
   echo -n '['
-  while [ $# -gt 0 ]; do
-    x=${1//\\/\\\\}
-    echo -n \"${x//\"/\\\"}\"
-    [ $# -gt 1 ] && echo -n ', '
-    shift
+  for crn in "$@"; do
+    echo -n \"${crn//\"/\\\"}\"
+    [ "$crn" != "${@: -1}" ] && echo -n ', '
   done
   echo ']'
 }
 
-arr=`json_array "${crn_list[@]}"`
+# Create the JSON array
+arr=$(json_array "${crn_list[@]}")
 
+# Define the URL (uncomment the one you want to use)
+url="${base_url}${test_endpoint}"
+# url="${base_url}${prod_endpoint}"
+
+# Make the API request
 curl -H "$token" -H "Content-Type: application/json" \
   --request POST --data-raw "{\"ECRN\": $arr, \"SCRN\":[]}"\
   $url | json_pp
